8cb848906d95ebdf95ec9f85324720f1f3e08ccc
Jazmin Nugra <jazzminn1748@gmail.com>
2021-06-14 04:16:48 +0000

    edit files

--- scripts/auth.js
+++ scripts/auth.js
@@ -28,6 +28,26 @@ signupForm.addEventListener('submit', (e) => {
     logout.addEventListener('click', (e) => {
       e.preventDefault();
       auth.signOut().then(() => {
-        console.log('User has signed out');
+        console.log('user signed out');
       })
+    });
+
+    //This is the login method
+    const loginForm = document.querySelector('#login-form');
+    loginForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      // get user info
+      const email = loginForm['login-email'].value;
+      const password = loginForm['login-password'].value;
+
+      // log the user in
+      auth.signInWithEmailAndPassword(email, password).then((cred) => {
+        console.log(cred.user);
+        // close the signup modal & reset form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+      });
+
     });
