49db9e6fd2990a81a8966b0a9c2f119cd6940171
Jazmin Nugra <jazzminn1748@gmail.com>
2021-06-14 04:19:48 +0000

    edit files

--- scripts/auth.js
+++ scripts/auth.js
@@ -24,30 +24,30 @@ signupForm.addEventListener('submit', (e) => {
 
 
 // This is the log out method
-    const logout = document.querySelector('#logout');
-    logout.addEventListener('click', (e) => {
-      e.preventDefault();
-      auth.signOut().then(() => {
-        console.log('user signed out');
-      })
-    });
+  const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+});
 
-    //This is the login method
-    const loginForm = document.querySelector('#login-form');
-    loginForm.addEventListener('submit', (e) => {
-      e.preventDefault();
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-      // get user info
-      const email = loginForm['login-email'].value;
-      const password = loginForm['login-password'].value;
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
 
-      // log the user in
-      auth.signInWithEmailAndPassword(email, password).then((cred) => {
-        console.log(cred.user);
-        // close the signup modal & reset form
-        const modal = document.querySelector('#modal-login');
-        M.Modal.getInstance(modal).close();
-        loginForm.reset();
-      });
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
 
-    });
+});
